c938162b795904a767222580202793a7d2dc9ab6
Nils Hamacher <hamacher.nils@stud.tu-darmstadt.de>
2019-06-27 16:19:49 +0200

    painting next poses into image working | somehow the pose-transformation isn't relative to the last postition. instead to the origin

--- constants.py
+++ constants.py
@@ -8,23 +8,28 @@ class Constants():
         ## quaternion rotation of coordinate system from world to camera system
         self.quats =[-0.5, 0.5, -0.5, 0.5]
 
-        self.theta = np.array(quaternion_to_euler_rad(self.quats), dtype=np.float32).reshape(3,1)
+        self.theta = np.array(quaternion_to_euler_rad(self.quats), dtype = np.float32).reshape(3, 1)
 
         ## camera intrinsics matrix
         #     [fx'  0  cx']
         # K = [ 0  fy' cy']
         #     [ 0   0   1 ]
-        self.K = np.array([718.856, 0.0, 607.1928, 0.0, 718.856, 185.2157, 0.0, 0.0, 1.0], dtype=np.float32).reshape(3,3)
+        self.K = np.array([718.856, 0.0, 607.1928,
+                           0.0, 718.856, 185.2157,
+                           0.0, 0.0, 1.0], dtype = np.float32).reshape(3, 3)
 
-        self.R = eulerAnglesToRotationMatrixRad(self.theta)
+        #self.R = eulerAnglesToRotationMatrixRad(self.theta)
+        self.R = np.eye(3, dtype = np.float)
 
         ## Translation from left & right wheel to camera
-        self.T_left = np.array([-1.68, 0.8, 1.65], dtype=np.float32).reshape(3,1)
-        self.T_right = np.array([-1.68, -0.8, 1.65], dtype=np.float32).reshape(3,1)
+        self.T_left = np.array([0.8, 1.65, 1.68], dtype = np.float32).reshape(3, 1)
+        self.T_right = np.array([-0.8, 1.65, 1.68], dtype = np.float32).reshape(3, 1)
 
         ## Transformationmatrices from left & right wheel to camera
-        self.RT_left = np.column_stack((self.R,self.T_left))
-        self.RT_right = np.column_stack((self.R,self.T_right))
+        self.RT_left = np.column_stack((self.R, self.T_left))
+        print(self.RT_left)
+        self.RT_right = np.column_stack((self.R, self.T_right))
+        print(self.RT_right)
 
         ## Projection matrices from left & right wheel to image coordinates
         self.KRT_left = np.matmul(self.K, self.RT_left)
@@ -49,21 +54,14 @@ class Constants():
         self.poses = []
         self.image_path = []
 
-
-
     def readFileLists(self, img_path, sequence):
         self.image_path = img_path + "sequences/" + sequence + "/image_0/"
         list_of_files = os.listdir(self.image_path)
         for image_name in list_of_files:
             self.image_names.append(image_name)
-
         self.image_names.sort()
         self.poses = np.loadtxt(img_path + "poses/" + sequence + ".txt")
 
-
-    # TODO: make this working
     def getImageName(self, idx):
-        return self.img_path + self.image_names[idx]
+        return self.image_path + self.image_names[idx]
 
-    def getPclName(self, idx):
-        return self.img_path + self.pcl_names[idx]
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import cv2
 import numpy as np
 import os
-
-from dateutil.rrule import weekday
-
 from constants import Constants
 from mathHelpers import add_ones
 
-kitti_dir = '/home/nils/nils/kitti/data_odometry_gray/dataset/'
+#kitti_dir = '/home/nils/nils/kitti/data_odometry_gray/dataset/'
+kitti_dir = '/media/localadmin/New Volume/11Nils/kitti/dataset/'
 sequence = '00'
 
 consts = Constants()
@@ -15,50 +13,69 @@ consts = Constants()
 consts.readFileLists(kitti_dir, sequence)
 
 image = cv2.imread(consts.image_path + consts.image_names[0])
-cv2.imshow("test", image)
+#cv2.imshow("test", image)
+print("\nimage shape: \n")
 height, width, channels = image.shape
 print(image.shape)
 labeled_image= np.zeros((height,width,3), np.uint8)
 
-pt = np.array((0.0, 0.0, 0.0), dtype=np.float).reshape(3, 1)
+pt = np.array((0.0, 0.0, 0.0), dtype = np.float).reshape(3, 1)
+pose_tf = np.eye(4, dtype = np.float)
 
 i = 0
-k = 20
-while i < k:
+j = 0
+k = 10
+while i < len(consts.image_names) - 1:
+    image = cv2.imread(consts.image_path + consts.image_names[i])
     j = i
-    pose_tf = add_ones(np.array(consts.poses[j]).reshape(3,4))
-    while j < k:
+    pt = np.array((0.0, 0.0, 0.0), dtype = np.float).reshape(3, 1)
+    labeled_image= np.zeros((height,width,3), np.uint8)
+    pose_chunk = np.eye(4, dtype = np.float)
+    last_pose = pose_chunk
+
+    while j < i + k:
         # concatenate the posetransformations first before multiplying with pt and K
-        pose_tf[:3, :3] = np.array(consts.poses[j]).reshape(3,4)
+        pose_chunk[:3, :4] = np.array(consts.poses[j]).reshape(3, 4)
+        print("pose_chunk \n")
+        print(pose_chunk)
+        #pose_tf = np.matmul(pose_tf, pose_chunk)
+        #print("\npose_conc \n")
+        #print(pose_tf)
+
         # reshaping pt to 4x1
         pt = np.array((pt[0], pt[1], pt[2], 1.0), dtype = np.float).reshape(4, 1)
-        # concatenating forward posetransformation
-        pt = np.matmul(pose_tf, pt)
-        print("pt")
-        print(pt)
-        print("\n")
+        # concatenating forward pose transformation to point
+        pt = np.matmul(pose_chunk, pt)
+        #print("pt")
+        #print(pt)
+        #print("\n")
         # projection into image coordinates
-        uvw = np.matmul(consts.KRT_left, np.array((pt[0], pt[1], pt[2], 1.0), dtype = np.float).reshape(4, 1))
-        print("uvw")
-        print(uvw)
-        print("\n")
-        if uvw[2] != 0:
+        uvw_l = np.matmul(consts.KRT_left, np.array((pt[0], pt[1], pt[2], 1.0), dtype = np.float).reshape(4, 1))
+        uvw_r = np.matmul(consts.KRT_right, np.array((pt[0], pt[1], pt[2], 1.0), dtype = np.float).reshape(4, 1))
+        #print("uvw")
+        #print(uvw)
+        #print("\n")
+        if uvw_l[2] != 0:
             #         x = u / w
             #         y = v / w
-            u = int(np.divide(uvw[0], uvw[2]))
-            v = int(np.divide(uvw[1], uvw[2]))
-            print(u, v)
-            if u > 0 and u < height:
-                if v > 0 and v < width:
-                    print(u, v)
-                    cv2.circle(labeled_image, (v, u), 2,(0, 0, 255), -1)
+            u = int(np.divide(uvw_l[0], uvw_l[2]))
+            v = int(np.divide(uvw_l[1], uvw_l[2]))
+            if u > 0 and u < width:
+                if v > 0 and v < height:
+                    cv2.circle(labeled_image, (u, v), 2, (0, 0, 255), -1)
+            u = int(np.divide(uvw_r[0], uvw_r[2]))
+            v = int(np.divide(uvw_r[1], uvw_r[2]))
+            if u > 0 and u < width:
+                if v > 0 and v < height:
+                    cv2.circle(labeled_image, (u, v), 2, (0, 255, ), -1)
+        j += 1
 
-
+    vis = cv2.addWeighted(image, 1.0, labeled_image, 1.0, 0.0)
+    cv2.imshow("vis", vis)
+    cv2.waitKey(100)
     i += 1
 
-#cv2.imshow("labeled", labeled_image)
 
-vis = cv2.addWeighted(image, 1.0, labeled_image, 1.0, 0.0)
-cv2.imshow("vis", vis)
-cv2.waitKey(2500) # in ms
+#cv2.imshow("vis", vis)
+#cv2.waitKey(0)   # in ms
 
