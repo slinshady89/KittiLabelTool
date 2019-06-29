853539c9d7e4c1b41ab74f4f01118d0beb1b751b
nils <hamacher.nils@gmail.com>
2019-06-29 18:04:02 +0200

    projection working. shift if projection of previous image is used. further investigation needed

--- main.py
+++ main.py
@@ -24,79 +24,108 @@ pose_tf = np.eye(4, dtype = np.float)
 
 i = 0
 j = 0
-k = 10
+k = 20
 while i < len(consts.image_names) - 1:
     image = cv2.imread(consts.image_path + consts.image_names[i])
     j = i
+    l = i - 1
     pt = np.array((0.0, 0.0, 0.0), dtype = np.float).reshape(3, 1)
+    pt_1 = np.array((0.0, 0.0, 0.0), dtype = np.float).reshape(3, 1)
     labeled_image= np.zeros((height, width, 3), np.uint8)
     pose_chunk = np.eye(4, dtype = np.float)
     pose_chunk[:3, :4] = np.array(consts.poses[i]).reshape(3, 4)
+
     inv_image_pose = np.linalg.inv(pose_chunk)
-    u_l_last = v_l_last = u_r_last = v_r_last = -1
+    pose_chunk_t1 = np.eye(4, dtype = np.float)
+    if l > 1:
+        pose_chunk_t1[:3, :4] = np.array(consts.poses[l]).reshape(3, 4)
+        inv_image_pose_t1 = np.linalg.inv(pose_chunk_t1)
+
+    pt_r_last = [-1, -1]
+    pt_l_last = [-1, -1]
     while j < i + k:
         # concatenate the posetransformations first before multiplying with pt and K
         if j > len(consts.image_names):
             break
+        I = np.eye(4)
         pose_chunk[:3, :4] = np.array(consts.poses[j]).reshape(3, 4)
+        I = pose_chunk
         pose_chunk = np.matmul(inv_image_pose, pose_chunk)
-
-
-        #print("pose_chunk \n")
-        #print(pose_chunk)
-        #pose_tf = np.matmul(pose_tf, pose_chunk)
-        #print("\npose_conc \n")
-        #print(pose_tf)
-
+        if pose_chunk[1][1] < 0.95:
+            print ("\n")
+            print(pose_chunk)
+            print ("\n")
+        inv_image_pose = np.linalg.inv(I)
         # reshaping pt to 4x1
         pt = np.array((pt[0], pt[1], pt[2], 1.0), dtype = np.float).reshape(4, 1)
         # concatenating forward pose transformation to point
         pt = np.matmul(pose_chunk, pt)
-        #print("pt")
-        #print(pt)
-        #print("\n")
+
         # projection into image coordinates
         uvw_l = np.matmul(consts.KRT_left, np.array((pt[0], pt[1], pt[2], 1.0), dtype = np.float).reshape(4, 1))
         uvw_r = np.matmul(consts.KRT_right, np.array((pt[0], pt[1], pt[2], 1.0), dtype = np.float).reshape(4, 1))
-        #print("uvw")
-        #print(uvw)
-        #print("\n")
         if uvw_l[2] != 0:
             #         x = u / w
             #         y = v / w
-            u_l = int(np.divide(uvw_l[0], uvw_l[2]))
-            v_l = int(np.divide(uvw_l[1], uvw_l[2]))
-            u_r = int(np.divide(uvw_r[0], uvw_r[2]))
-            v_r = int(np.divide(uvw_r[1], uvw_r[2]))
+            pt_l = (int(np.divide(uvw_l[0], uvw_l[2])), int(np.divide(uvw_l[1], uvw_l[2])))
+            pt_r = (int(np.divide(uvw_r[0], uvw_r[2])), int(np.divide(uvw_r[1], uvw_r[2])))
             left_in = 0
             right_in = 0
 
-            if u_l > 0 and u_l < width:
-                if v_l > 0 and v_l < height:
-                    left_in = 1
-                    print ("\nleft")
-                    print("\nu and v \n")
-                    print(u_l, v_l)
-                    cv2.circle(labeled_image, (u_l, v_l), 3, (0, 0, 255), -1)
-            if u_r > 0 and u_r < width:
-                if v_r > 0 and v_r < height:
-                    print("\nright")
-                    print("\nu and v \n")
-                    print(u_r, v_r)
-                    right_in = 1
-                    cv2.circle(labeled_image, (u_r, v_r), 3, (0, 255,), -1)
-
+            if pt_l[0] > 0 and pt_l[0] < width and pt_l[1] > 0 and pt_l[1] < height:
+                left_in = 1
+                cv2.circle(labeled_image, pt_l, 3, (0, 0, 255), -1)
+            if pt_r[0] > 0 and pt_r[0] < width and pt_r[1] > 0 and pt_r[1] < height:
+                right_in = 1
+                cv2.circle(labeled_image, pt_r, 3, (0, 255,), -1)
             if left_in == 1 and right_in == 1:
-                rect = np.array([[u_l, v_l], [u_r, v_r], [u_r_last, v_r_last], [u_l_last, v_l_last]], np.int32).reshape((-1, 1, 2))
+                rect = np.array([pt_l, pt_r, pt_r_last, pt_l_last], np.int32).reshape((-1, 1, 2))
                 cv2.fillPoly(labeled_image, [rect], (0, 255, 0))
-                #cv2.rectangle(labeled_image, (u_l, u_l_last), (u_r, u_r_last), (0, 0, 255), cv2.FILLED)
             if j > i:
-                u_l_last = u_l
-                v_l_last = v_l
-                u_r_last = u_r
-                v_r_last = v_r
-        j += 1
+               pt_l_last = pt_l
+               pt_r_last = pt_r
 
+        j += 1
+    if l > 1:
+        while l < i + k:
+            # concatenate the posetransformations first before multiplying with pt and K
+            if l > len(consts.image_names):
+                break
+            I = np.eye(4)
+            pose_chunk[:3, :4] = np.array(consts.poses[l]).reshape(3, 4)
+            I = pose_chunk
+            pose_chunk = np.matmul(inv_image_pose, pose_chunk)
+            inv_image_pose = np.linalg.inv(I)
+            # reshaping pt to 4x1
+            pt = np.array((pt[0], pt[1], pt[2], 1.0), dtype = np.float).reshape(4, 1)
+            # concatenating forward pose transformation to point
+            pt = np.matmul(pose_chunk, pt)
+
+            # projection into image coordinates
+            uvw_l = np.matmul(consts.KRT_left, np.array((pt[0], pt[1], pt[2], 1.0), dtype = np.float).reshape(4, 1))
+            uvw_r = np.matmul(consts.KRT_right, np.array((pt[0], pt[1], pt[2], 1.0), dtype = np.float).reshape(4, 1))
+            if uvw_l[2] != 0:
+                #         x = u / w
+                #         y = v / w
+                pt_l = (int(np.divide(uvw_l[0], uvw_l[2])), int(np.divide(uvw_l[1], uvw_l[2])))
+                pt_r = (int(np.divide(uvw_r[0], uvw_r[2])), int(np.divide(uvw_r[1], uvw_r[2])))
+                left_in = 0
+                right_in = 0
+
+                if pt_l[0] > 0 and pt_l[0] < width and pt_l[1] > 0 and pt_l[1] < height:
+                    left_in = 1
+                    cv2.circle(labeled_image, pt_l, 3, (0, 0, 255), -1)
+                if pt_r[0] > 0 and pt_r[0] < width and pt_r[1] > 0 and pt_r[1] < height:
+                    right_in = 1
+                    cv2.circle(labeled_image, pt_r, 3, (0, 255,), -1)
+                if left_in == 1 and right_in == 1:
+                    rect = np.array([pt_l, pt_r, pt_r_last, pt_l_last], np.int32).reshape((-1, 1, 2))
+                    cv2.fillPoly(labeled_image, [rect], (0, 0, 150))
+                if l > i:
+                    pt_l_last = pt_l
+                    pt_r_last = pt_r
+
+            l += 1
     vis = cv2.addWeighted(image, 1.0, labeled_image, 1.0, 0.0)
     cv2.imshow("vis", vis)
     cv2.waitKey(100)
