143e5cf722e74d05fa20259b82ef769d9f64fdf7
Nils Hamacher <hamacher.nils@stud.tu-darmstadt.de>
2019-07-10 15:46:43 +0200

    added some comments | rewrote drawing process | fixed roll adjustment error

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+
 import cv2
 import numpy as np
 import os
+
 from constants import Constants
 from mathHelpers import pt_in_image, rotationMatrixToEulerAngles
 import transformations as tf
 
-kitti_dir = '/home/nils/nils/kitti/data_odometry_gray/dataset/'
-#kitti_dir = '/media/localadmin/New Volume/11Nils/kitti/dataset/'
+#kitti_dir = '/home/nils/nils/kitti/data_odometry_gray/dataset/'
+kitti_dir = '/media/localadmin/New Volume/11Nils/kitti/dataset/'
 sequence = '00'
-velo_dir = '/home/nils/nils/kitti/dataset/sequences/'
-#velo_dir = '/media/localadmin/New Volume/11Nils/kitti/dataset/sequences/'
+#velo_dir = '/home/nils/nils/kitti/dataset/sequences/'
+velo_dir = '/media/localadmin/New Volume/11Nils/kitti/dataset/sequences/'
 
 # https://github.com/hunse/kitti/blob/master/kitti/velodyne.py
 def load_velodyne_points(drive,  frame):
@@ -22,8 +24,12 @@ def processPointCloud(img, pointcloud, pitch, roll, detections, div = 5):
     i = 0
     usedPoints = 0
     mean_z = 0
+
     height, width, channels = img.shape
     processedImg = np.zeros((height, width, 3), np.uint8)
+
+    blue_rect = np.array([[0, 0], [0, height - 1], [width - 1, height - 1], [width - 1, 0]], np.int32).reshape((-1, 1, 2))
+    cv2.fillPoly(processedImg, [blue_rect], (255, 0, 0))
     while i < len(pointcloud):
         x = pointcloud[i][0]
         y = pointcloud[i][1]
@@ -46,15 +52,12 @@ def processPointCloud(img, pointcloud, pitch, roll, detections, div = 5):
 
                         if 0 < u < width - 1 and 0 < v < height:
                             # height of the lidar relative to the street on a plane
-                            ground_depth = -1.73
                             # if vehicle has pitch and roll angles these change the vehicle relative groundplane
-                            if not y == 0:
-                                ground_depth -= np.sin(pitch) / np.sqrt(x * x + y * y) + np.sin(roll) / y
-                            else:
-                                ground_depth -= np.sin(pitch) / np.sqrt(x * x + y * y)
+                            # for very small lateral distances the roll influence is wrongly increased
+                            ground_depth = -1.73 - np.sin(pitch) / np.sqrt(x * x + y * y) - y * np.arctan(roll)
 
                             if z > ground_depth + 0.3:
-                                if np.abs(detections[u // div, 0] - v) < 5 or detections[u // div, 0] == 0:
+                                if np.abs(detections[u // div, 0] - v) < 10 or detections[u // div, 0] == 0:
                                     detections[u // div, 0] = v
                                     detections[u // div, 1] += 1
                                     detections[u // div, 2] = 1 - 0.4 ** detections[u // div, 1]
@@ -66,12 +69,43 @@ def processPointCloud(img, pointcloud, pitch, roll, detections, div = 5):
 
         i += 1
     mean_prob = 0
+
+    draw_left = True
+
+    # draw rectangles from the lower left bounder of the left reflection until the column of the next reflection
     for u in range(0, len(detections) - 1):
+        # accumulate probability of existence for every grid cell
         mean_prob += detections[u, 2]
         v = int(detections[u, 0])
-        if detections[u, 2] > 0.5:
-            cv2.rectangle(processedImg, (u * div, v + 1), ((u + 1) * div, height), (255, 0, 0), 3)
-            cv2.rectangle(processedImg, (u * div, v), ((u + 1) * div, 0), (0, 0, 255), 3)
+        th_detect = 0.94  # > 1-0.4**4
+        if detections[u, 2] > th_detect:
+            left_refl = np.array([u * div, v + 1])
+            right_refl = np.array([u * div, v + 1])
+
+            # search for the next reflection which probability of existence is higher than th_detect (0.85)
+            for m in range(u + 1, len(detections) - 1):
+                if detections[m, 2] > th_detect:
+                    right_refl = np.array([m * div, v + 1])
+                    break
+            # draw from left side of the image until the first reflection
+            if draw_left:
+                refl_rect = np.array([[0, 0], [left_refl[0], 0], left_refl, [0, left_refl[1]]], np.int32).reshape((-1, 1, 2))
+                cv2.fillPoly(processedImg, [refl_rect], (0, 0, 255))
+                draw_left = False
+
+
+            bottom_left = left_refl
+            top_left = [left_refl[0], 0]
+            bottom_right = right_refl
+            top_right = [right_refl[0], 0]
+            refl_rect = np.array([top_left, bottom_left, bottom_right, top_right], np.int32).reshape((-1, 1, 2))
+            cv2.fillPoly(processedImg, [refl_rect], (0, 0, 255))
+
+    # fill the free space right of the most right reflection
+    refl_rect = np.array([bottom_right, top_right, [width - 1, 0], [width - 1, bottom_right[0]]], np.int32).reshape((-1, 1, 2))
+    cv2.fillPoly(processedImg, [refl_rect], (0, 0, 255))
+
+    # print mean probability of existence for all grid cells
     print(mean_prob / len(detections))
     return processedImg, detections
 
@@ -96,53 +130,40 @@ consts.readTfLidarToCamera0(kitti_dir, sequence)
 i = 0
 j = 0
 # look ahead
-k = 40
+k = 100
 
-# should result in turn of 30 deg around X-Axis
-test_R = np.array([1, 0, 0, 0, np.sqrt(3) / 2, 0.5, 0, -0.5, np.sqrt(3) / 2]).reshape(3, 3)
-print(test_R)
-print(rotationMatrixToEulerAngles(test_R)*180 / 3.14159)
-test = np.eye(4)
-test[:3, :3] = test_R #* np.array([0, 0, 1, -1, 0, 0, 0, -1, 0]).reshape(3, 3)
-print(test)
-alpha, beta, gamma = tf.euler_from_matrix(test, 'rzyx')
-print("\n zyx system \n")
-print(alpha * 180 / 3.1415, beta * 180 / 3.1415, gamma * 180 / 3.1415)
+divisor = 5
 
-detections = np.zeros((width // 5, 3), np.float)
-q_C2Veh = [0.500, -0.500, 0.500, -0.500]
+detections = np.zeros((width // divisor, 3), np.float)
 
 while i < len(consts.image_names) - 1:
+    print(i)
     image = cv2.imread(consts.image_path + consts.image_names[i])
     j = i
     pt = np.array((0.0, 0.0, 0.0), dtype = np.float).reshape(3, 1)
     pose_chunk = np.eye(4, dtype = np.float)
     pose_chunk[:3, :4] = np.array(consts.poses[i]).reshape(3, 4)
 
-    quat = tf.quaternion_from_matrix(pose_chunk)
-    yaw, roll, pitch = tf.euler_from_quaternion(quat, 'ryzx') #ryzx or rxzy
-    print(yaw * 180 / 3.1415, roll * 180 / 3.1415, pitch * 180 / 3.1415)
+    yaw, roll, pitch = tf.euler_from_matrix(pose_chunk, 'ryzx') #ryzx or rxzy
+    #print(yaw * 180 / 3.1415, roll * 180 / 3.1415, pitch * 180 / 3.1415)
 
     last_detections = detections
     points = load_velodyne_points(velo_dir + sequence, i)
-    labeled_image, detections = processPointCloud(image, points, pitch, roll, detections, 5)
+    labeled_image, detections = processPointCloud(image, points, pitch, roll, detections, divisor)
     #labeled_image = np.zeros(image.shape, dtype = np.uint8)
 
-    #'''
-    for k in range(0, len(detections)):
-        if detections[k, 1] == last_detections[k, 1]:
-            if detections[k, 1] > 0:
-                detections[k, 2] -= 0.4 ** detections[k, 1]
-                detections[k, 1] -= 1
-            if detections[k, 1] == 0:
-                detections[k, 0] = 0
-                detections[k, 2] = 0.0
-    #'''
+    # aging of detections
+    for l in range(0, len(detections)):
+        if detections[l, 1] == last_detections[l, 1]:
+            if detections[l, 1] > 0:
+                detections[l, 2] -= 0.4 ** detections[l, 1]
+                detections[l, 1] -= 1
+            if detections[l, 1] == 0:
+                detections[l, 0] = 0
+                detections[l, 2] = 0.0
+
 
     inv_image_pose = np.linalg.inv(pose_chunk)
-    #test = np.matmul(consts.C2Veh, pose_chunk[:3, :3])
-    #y, p, r = rotationMatrixToEulerAngles(pose_chunk[:3, :3])
-    #print(y * 180 / 3.14159, p * 180 / 3.14159, r * 180 / 3.14159)
 
     pt_r_last = [-1, -1]
     pt_l_last = [-1, -1]
@@ -175,10 +196,10 @@ while i < len(consts.image_names) - 1:
 
             if pt_in_image(pt_l, width, height):
                 left_in = 1
-                cv2.circle(labeled_image, pt_l, 1, (0, 255, 0), -1)
+                #cv2.circle(labeled_image, pt_l, 1, (0, 255, 0), -1)
             if pt_in_image(pt_r, width, height):
                 right_in = 1
-                cv2.circle(labeled_image, pt_r, 1, (0, 255,0), -1)
+                #cv2.circle(labeled_image, pt_r, 1, (0, 255,0), -1)
 
             if pt_in_image(pt_l_last, width, height) and pt_in_image(pt_r_last, width, height):
                 if not pt_l_last == (-1, -1) or pt_r_last == (-1, -1):
@@ -187,8 +208,8 @@ while i < len(consts.image_names) - 1:
             if j > i:
                pt_l_last = pt_l
                pt_r_last = pt_r
-
         j += 1
+
     vis = cv2.addWeighted(image, 1.0, labeled_image, 1.0, 0.0)
     cv2.imshow("gt", image)
     cv2.imshow("vis", vis)
